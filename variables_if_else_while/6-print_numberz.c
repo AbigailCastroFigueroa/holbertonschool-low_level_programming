e917432d86d5a80c98553ca90f68cdd2d9f4fed3
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-07-21 01:55:32 +0000

    Uploading the 6-print_numberz.c file to github

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
