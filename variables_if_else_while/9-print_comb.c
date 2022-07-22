edaa7d061ab06b758cfa8c143b1e974427a2a0a1
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-07-22 00:03:18 +0000

    Uploading the 9-print_comb.c file to github

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		putchar(44);
+		putchar(32);
+	}
+	putchar(10);
+	return (0);
+}
