77afc2f70b41d911d864e1a186e9aa3a4aceda3e
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-07-23 17:43:14 +0000

    Uploading a preliminar phase before discussing it in the PLD

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -5,20 +5,21 @@
  */
 int main(void)
 {
-	int x = 0;
-	int y = 0;
+	int x, y;
 
 	for (x = 48; x <= 57; x++)
+	{
 		for (y = 49; y <= 57; y++)
 		{
-			putchar(x);
-			putchar(y);
 		if (x < y && x != y)
 			{
+			putchar(x);
+			putchar(y);
 			putchar(44);
 			putchar(32);
 			}
 		}
+	}
 	putchar(10);
 	return (0);
 }
