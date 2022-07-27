1ec5b55d07c1c62d67cb0ee36299dd886d2bf728
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-07-27 01:26:32 +0000

    Uploading the 1-main.c and the 1-alphabet.c files to github.

---
+++ functions_nested_loops/1-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - check the code.
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char a = 0;
+
+	for (a = 97; a <= 122; a++)
+	{
+		_putchar(a);
+	}
+	_putchar(10);
+}
