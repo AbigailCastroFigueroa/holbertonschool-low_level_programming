df07cf596d9dabe7fc543f75a3f4a7cea3db7bd0
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-07-28 01:16:30 +0000

    uploading the 6-abs.c file to github

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - print the absolute value of a given integer.
+ * @i: integer value.
+ *
+ *  Return: 0
+ */
+int _abs(int i)
+{
+	if (i  < 0)
+		i = -i;
+	return (i);
+}
