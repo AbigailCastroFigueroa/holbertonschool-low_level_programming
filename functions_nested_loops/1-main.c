b4341e0c98f5f6f1b1b787414faf232bf8cf2ef2
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-07-28 00:11:21 +0000

    only god knows

--- functions_nested_loops/1-main.c
+++ functions_nested_loops/1-main.c
@@ -1,17 +1,12 @@
 #include "main.h"
+
 /**
- * main - check the code.
+ * main - check the code for Holberton School students.
  *
- * Return: Always 0
+ * Return: Always 0.
  */
-
-void print_alphabet(void)
+int main(void)
 {
-	char a = 0;
-
-	for (a = 97; a <= 122; a++)
-	{
-		_putchar(a);
-	}
-	_putchar(10);
+	print_alphabet();
+	return (0);
 }
