1816e191672e5ce8c8d6b6082d46d37d8f654f09
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-07-29 00:56:35 +0000

    Uploading the 10-add.c file to github.

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - present the sum of two given variables.
+ * @x: first argument.
+ * @y: second argument.
+ *
+ * Return: sum of the value of the given variables.
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
