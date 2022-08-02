69052f47ae1b5cfd6a4531f8cf412effe5b18e16
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-08-02 03:07:48 +0000

    Uploading the 9-fizz_buzz.c file to github

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int i = '1';
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (! (i % 15))
+			printf("FizzBuzz ");
+
+		else if (! (i % 3))
+			printf("Fizz ");
+
+		else if (! (i % 5))
+			printf("Buzz ");
+
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
