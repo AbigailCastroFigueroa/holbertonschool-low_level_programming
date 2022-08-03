9ffb2e7d341c4b5ac8179815e18a1a0be28902de
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-08-03 02:41:49 +0000

    Fixing when size is less than 0 output

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -7,11 +7,19 @@ void print_square(int size)
 {
 	int a, b;
 
-	for (a = 0; a < size; a++)
+	if (size > 0)
 	{
-		for (b = 0; b < size; b++)
-		_putchar(35);
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+				_putchar(35);
 
 			_putchar(10);
+		}
+	}
+
+	else
+	{
+		_putchar(10);
 	}
 }
