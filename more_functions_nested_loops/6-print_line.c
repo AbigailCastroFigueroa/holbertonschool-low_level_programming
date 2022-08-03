f149f57852702899853fa35166644338bc507e2e
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-08-03 00:26:29 +0000

    Uploading the 6-print_line.c file to github

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line in the terminal.
+ *@n: value given.
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n < 0)
+		_putchar(10);
+
+	for (i = 0; i < n; i++)
+		_putchar(95);
+}
