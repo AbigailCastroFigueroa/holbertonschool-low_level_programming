018da116e329a5ee9b1c45bababc7e9b970cbf49
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-08-06 10:39:54 +0000

    Corrections to Betty, adding space in both sides of the '/' character

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -5,14 +5,16 @@
  */
 void print_number(int n)
 {
+	unsigned int c;
+
 	if (n < 0)
 	{
 		_putchar(45);
 		   n = -n;
 	}
 
-	if (n/10)
-		print_number(n/10);
+	if (n / 10)
+		print_number(n / 10);
 
 	_putchar((n % 10) + '0');
 }
