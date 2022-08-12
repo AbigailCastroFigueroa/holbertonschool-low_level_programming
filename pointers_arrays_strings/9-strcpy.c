81bb79c7927fdee9b164f8aa0a3da96b5f790db9
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-08-12 01:13:05 +0000

    Uploading the 9-strcpy.c file to github

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * strcpy - copies a string pointed including the null
+ * @src: original string.
+ * @dest: copied string.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a = 0;
+	int i;
+
+	while (src[a] != 0)
+		a++;
+
+	for (i = 0; i < a && src[i] != 0; i++)
+	{
+		dest[i] = src[i];
+	}
+		for ( ; i < a; i++)
+		{
+			dest[i] = 0;
+	}
+	return (dest);
+}
