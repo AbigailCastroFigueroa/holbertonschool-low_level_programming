b3cd1610c4da8f58ae373b2d79b41e0eb41926d7
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-08-19 00:59:12 +0000

    uploading the 3-strcmp.c file to github

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcmp - function that compare two given strings.
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 0 if both are equals, the ascii difference
+ *         between the first unmatched tokens.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != s2[i]; i++)
+		if (s1 != 0)
+		{
+			return (s1[i] - s2[i]);
+		}
+	return (0);
+}
