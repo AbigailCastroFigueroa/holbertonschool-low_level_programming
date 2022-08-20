8d4bb8eb914faf1d4b04425abbdadedefaa9be76
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-08-20 15:57:22 +0000

    Removing the index references from the conditions s1[i] -> s1

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -11,8 +11,10 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; s1[i] != s2[i]; i++)
-		if (s2 != 0 && s1 != 0)
-			return (s1[i] - s2[i]);
-	return (0);
+	for (i = 0; i != 0; i++)
+	{
+		if (s1 == s2)
+			return (0);
+	}
+	return (s1 - s2);
 }
