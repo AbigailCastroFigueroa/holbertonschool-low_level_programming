505aa3520fcc6a6b77f827a87b00f7855eeaae61
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-09-06 01:56:50 +0000

    Fixing code, separating the if statements, taking the if (size == 0) statement out of the loop

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -15,9 +15,14 @@ char *create_array(unsigned int size, char c)
 
 	ar = malloc(size * sizeof(char));
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
 	for (i = 0; i < size; i++)
 	{
-		if (size == 0 || ar == 0)
+		if (ar == 0)
 		{
 			return (NULL);
 		}
