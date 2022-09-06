82863798c2b8396d99e898679b53ad090b0d40b6
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-09-06 02:32:07 +0000

    Adding if statement, if str == 0 return NULL...before even start working with the pointer or malloc

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -12,12 +12,16 @@ char *_strdup(char *str)
 {
 	char *ptr;
 
-	ptr = (char *) malloc(strlen(str) + 1);
 	if (str == 0)
 	{
 		return (NULL);
 	}
 
+	ptr = (char *) malloc(strlen(str) + 1);
+	if (ptr == 0)
+	{
+		return (NULL);
+	}
 	if (ptr != 0)
 	{
 		strcpy(ptr, str);
