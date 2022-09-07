630821402e57b3bae95cd9604c8781ea9a413729
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-09-07 01:30:39 +0000

    Adding instructions for when one of the strings are equal NULL pass as an empty string

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,6 +13,11 @@ char *str_concat(char *s1, char *s2)
 {
 	char *ptr;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	ptr = (char *)malloc(strlen(s1) + strlen(s2) + 1);
 	if (ptr == 0)
 	{
