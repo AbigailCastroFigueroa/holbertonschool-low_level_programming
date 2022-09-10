36ac40a0c9c012278a271df33b0a52dc0a635a24
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-09-10 18:40:04 -0400

    Uploading the 1-string_nconcat.c file to github

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * string_nconcat - merge two strings.
+ * @s1: first string.
+ * @s2: second string.
+ * @n: amount of bytes of the second string to add to first string.
+ *
+ * Return: The resulting string after merge.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i1, i2, iptr, size0 = 0, size1 = 0;
+	char *ptr;
+
+	for (i1 = 0; s1[i1] != 0; i1++)
+		size0++;
+	for (i2 = 0; s2[i2] != 0; i2++)
+		size1++;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	ptr = malloc((size0 + n + 1) * sizeof(char));
+
+	if (size1 <= n)
+		n = size1;
+
+	if (ptr == NULL)
+		return (NULL);
+	for (iptr = 0; iptr < size0 + n; iptr++)
+	{
+		if (*s1)
+		{
+			ptr[iptr] = *s1;
+			s1++;
+		}
+		else
+		{
+		ptr[iptr] = *s2;
+		s2++;
+		}
+	}
+	ptr[iptr] = 0;
+	return (ptr);
+}
+
