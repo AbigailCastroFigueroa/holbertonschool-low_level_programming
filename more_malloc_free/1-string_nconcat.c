d33ae8672cc0d143475e5f90f54ad0170748fda9
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-09-10 18:58:44 -0400

    Switching place with the for loops

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -14,15 +14,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i1, i2, iptr, size0 = 0, size1 = 0;
 	char *ptr;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	for (i1 = 0; s1[i1] != 0; i1++)
 		size0++;
 	for (i2 = 0; s2[i2] != 0; i2++)
 		size1++;
 
-	if (s1 == NULL)
-		s1 = " ";
-	if (s2 == NULL)
-		s2 = " ";
 	ptr = malloc((size0 + n + 1) * sizeof(char));
 
 	if (size1 <= n)
