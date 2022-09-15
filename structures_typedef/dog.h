172c7fd0b8c4afb4e4f528585d0fa0917a1ade54
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-09-14 20:23:26 -0400

    Uploading the dog.h file to github

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - it let you know the dog name, age and owner
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: let you know the dog age, name and owner.
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
