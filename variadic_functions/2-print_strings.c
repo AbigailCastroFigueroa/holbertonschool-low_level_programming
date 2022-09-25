430083c43a940bb10bc201d934352198d008de08
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-09-25 18:32:39 -0400

    Uploading the 2-print_strings.c file to github

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - print strings followed by a new line
+ * @separator: separate arguments given
+ * @n: amount of arguments given
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list string;
+	char *str;
+
+	va_start(string, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(string, char *);
+		if (!str)
+		{
+			printf("nil");
+		}
+		printf("%s", str);
+		if (i != n - 1 && separator)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+}
