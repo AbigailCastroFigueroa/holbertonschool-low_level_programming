364be1248986b9a5e47dc1f4eae98eea26b8d9fe
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-10-22 12:05:59 -0400

    Uploading the 100-first.c file to github for the first time

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * before_main - prints content before main is executed.
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
