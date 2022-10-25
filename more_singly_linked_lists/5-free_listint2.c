cc53cc29e6898288d610c171117a865c6890d318
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-10-25 19:13:16 -0400

    Uploading the 5-free_listint2.c file to github for the first time

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees linked list and reset head to NULL
+ * @head: linked list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head)
+	{
+		temp = *head;
+		*head = temp->next;
+		free(temp);
+	}
+	*head = NULL;
+}
+
+
+
