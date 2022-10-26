bbcb3645c21e24ef28e23e0aacd2f26cd2bb4c52
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-10-26 19:54:54 -0400

    *head = new_node if list it's empty

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -22,9 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (*head == NULL)
 	{
-		temp = *head;
-		new_node->next = temp->next;
-		temp->next = new_node;
+		*head = new_node;
 	}
 	else
 	{
