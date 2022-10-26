43b9f50e019ff9864c8e16162db35ef34c05aa15
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-10-25 20:57:42 -0400

    Uploading the 6-pop_listint.c file to github for the first time

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * pop_listint - delete a head node
+ * @head: linked list
+ *
+ * Return: delete node data
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int value = (*head)->n;
+
+	temp = *head;
+	*head = temp->next;
+	free(temp);
+	return (value);
+}
