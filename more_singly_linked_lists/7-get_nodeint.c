d443d1b6efc0e30435e74c5e6e872af6abaf61c3
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-10-26 13:50:38 -0400

    Uploading the 7-get_nodeint.c file to github for the first time

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * get_nodeint - get the node in the position selected
+ * @head: linked list
+ * @index: position selected in the linked list
+ *
+ * Return: node selected or NULL if it fails.
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *temp;
+
+	temp = head;
+
+	if (!temp)
+		return (NULL);
+	for (i = 0; i < index; i++)
+	{
+		temp = temp->next;
+	}
+	return (temp);
+}
+
