9d341967d9083aa75027d2ca19b362a25f728926
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-10-26 17:13:17 -0400

    Uploading the 9-insert_nodeint.c file to github for the first time

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - insert a new node at a given position
+ * @head: linked list
+ * @idx: position to insert the new node
+ * @n: data for the new node
+ *
+ * Return: the new node inserted or NULL if fails
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *temp, *new_node;
+	unsigned int i;
+
+	new_node = malloc(sizeof(listint_t));
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	temp = *head;
+
+	for (i = 1; i < idx; i++)
+	{
+		temp = temp->next;
+	}
+	new_node->next = temp->next;
+	temp->next = new_node;
+	return (*head);
+}
