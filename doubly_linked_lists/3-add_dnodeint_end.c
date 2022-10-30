be450e51b0e6a25026848bdd4f677daae47701aa
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-10-30 13:40:46 -0400

    Uploading the 3-add_dnodeint_end.c file to github for the first time

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - at a new node at the end of the list
+ * @head: head of the list
+ * @n: data for the new node
+ *
+ * Return: the address of the new node or NULL if fails.
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	int i;
+	dlistint_t *new, *temp = *head;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		for (i =  0; temp->next != 0; i++)
+			temp = temp->next;
+		temp->next = new;
+		new = temp->prev;
+		new = temp;
+	}
+	return (temp);
+}
