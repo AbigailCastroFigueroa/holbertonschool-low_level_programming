b249c540994a292daafaeab9e27c9e291442dcc3
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-10-31 18:47:12 -0400

    Uploading the 7-insert_dnodeint.c file to github for the first time

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - insert a new node at a given position
+ * @h: linked list
+ * @idx: position to insert the new node
+ * @n: data for the new node
+ *
+ * Return: the new node inserted or NULL if fails
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *temp, *new_node;
+	unsigned int i;
+
+	new_node = malloc(sizeof(dlistint_t));
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;
+
+	if (idx == 0)
+	{
+		new_node->next = *h;
+		*h = new_node;
+	}
+	else
+	{
+		temp = *h;
+
+		for (i = 0; i < idx - 1 && temp != 0; i++)
+		{
+			temp = temp->next;
+		}
+		if (temp == NULL)
+			return (NULL);
+		new_node->next = temp->next;
+		temp->next = new_node;
+	}
+	return (new_node);
+}
