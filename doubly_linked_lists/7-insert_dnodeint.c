084f46da350fc8dfc2513569a02947423f905dc8
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-11-05 16:19:01 -0400

    changing to return head

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -30,7 +30,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		(*h)->prev = new_node;
 		new_node->next = *h;
 		*h = new_node;
-		return (new_node);
+		return (*h);
 	}
 	else
 	{
