0750e604115c806da84251845aadfd247e5658d4
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-11-05 19:47:36 -0400

    condition if write fails

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -12,12 +12,10 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-	write (fd, text_content, strlen(text_content));
-	if (!text_content)
-		return (0);
-
+	if ((write(fd, text_content, strlen(text_content)) == -1))
+		return (-1);
 	close(fd);
 	return (1);
 }
