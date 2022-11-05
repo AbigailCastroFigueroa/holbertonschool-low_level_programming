17e69081943415d7dd15122130a728222ad6d7bb
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-11-05 19:39:28 -0400

    Changing permission to the file descriptor, plus adding if condition if text file is NULL

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,11 +10,13 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
 	if (!filename)
 		return (-1);
 	write (fd, text_content, strlen(text_content));
+	if (!text_content)
+		return (0);
 
 	close(fd);
 	return (1);
