ada66089ae9fab32db20f476e641657d9009cc0f
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-11-05 19:31:54 -0400

    Adding string.h library to header

--- file_io/main.h
+++ file_io/main.h
@@ -6,6 +6,7 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
