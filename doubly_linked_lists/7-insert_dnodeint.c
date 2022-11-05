354c22acad77f12cb51170d66a46696ad7745a73
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-11-05 12:21:33 -0400

    Returning head if head it's null and we add node at 0 index

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -24,7 +24,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		if (*h == NULL)
+		{
 			*h = new_node;
+			return (*h);
+		}
 		(*h)->prev = new_node;
 		new_node->next = *h;
 		*h = new_node;
