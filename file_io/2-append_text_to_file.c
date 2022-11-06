3c746db5ad1caff85953767e451607dd76826e57
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2022-11-05 20:30:44 -0400

    Returning 1 if text_content is equal NULL

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -18,8 +18,8 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if ((write(fd, text_content, strlen(text_content)) == -1))
 		return (-1);
-	if (!text_content)
-		close(fd);
+	if (text_content == NULL)
+		return (1);
 	close(fd);
 	return (1);
 }
