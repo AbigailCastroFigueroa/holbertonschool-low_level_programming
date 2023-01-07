05e749648ab375ce30baca095c6f299e528a3473
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-01-07 11:49:10 -0500

    Uploading the 0-linear.c file to github for the first time

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include "search_algos.h"
+/**
+ * linear_search - write a function that searches for a value in an array
+ *                 of integers using the linear search algorithm
+ * @array: array given
+ * @size: size of the array.
+ * @value: value to be search in the array.
+ *
+ * Return: the index where the value it's found, -1 if fails.
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	unsigned int i;
+
+	if (array == NULL)
+		return (-1);
+	for (i = 0; i < size && array[i] != value; i++)
+	{
+		printf("Valued checked array[%d] = [%d]\n", i, array[i]);
+	}
+	if (array[i] == value)
+	{
+		printf("Valued checked array[%d] = [%d]\n", i, array[i]);
+		return (i);
+	}
+	return (-1);
+}
+
