6e8fd8bc1582f8793704861fa25d543a0d00e1c5
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-01-21 13:23:52 -0500

    Adding whitespace before opening parenthesis in line 34

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -31,7 +31,7 @@ int binary_search(int *array, size_t size, int value)
 			right = half - 1;
 		else if (array[half] < value)
 			left = half + 1;
-		else if(value == array[half])
+		else if (value == array[half])
 			return (half);
 	}
 	return (-1);
